fedb80452d6581592223fc3e96f87890e71d7b34
IslanP1 <islanpereiradeoliveira@gmail.com>
2022-12-14 22:49:21 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
+import tmdbsimple as tmdb
 
 app = Flask(__name__)
-from views import *
+tmdb.API_KEY = ""
+
+
+@app.route('/')
+def index():
+    return 'Hello world!'
+
+
+@app.route("/movie/<title>")
+def movie(title):
+    results = tmdb.Search().movie(query=title)["results"]
+    
+    movie = results[0] if results else None
+
+    poster_url, overview, rating = None, None, None
+    if movie:
+        movie_id = movie["id"]
+        movie_details = tmdb.Movies(movie_id).info(language="pt-BR")
+    
+        poster_path = movie["poster_path"]
+        poster_url = "https://image.tmdb.org/t/p/w500{}".format(poster_path)
+
+        overview = movie_details["overview"]
+        rating = movie_details["vote_average"]
+        lancamento = movie_details["release_date"]
+        titulo = movie_details["title"]
+        popularidade = movie_details["popularity"]
+      
+
+    return render_template("movie.html", poster_url=poster_url, overview=overview, rating=rating, 
+                           lancamento=lancamento, titulo=titulo, popularidade=popularidade )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- views.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from main import app
-
-@app.route('/')
-def index():
-    return 'Hello world!'
